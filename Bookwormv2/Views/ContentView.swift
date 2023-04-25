56d3888ba814f962914401679495a4c0ae5a7dd7
Anthony Gibson <anthony.gibson@icloud.com>
2023-04-25 22:11:12 +0100

    cleaned contentView

--- Bookwormv2/Views/ContentView.swift
+++ Bookwormv2/Views/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
    
    @State private var showingAddScreen = false
    
-   
-   
    // Fetch Items from CoreData
    @FetchRequest(
       sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
@@ -68,22 +66,6 @@ struct ContentView: View {
       }
    }
    
-   private func addItem() {
-      withAnimation {
-         let newItem = Item(context: viewContext)
-         newItem.timestamp = Date()
-         
-         do {
-            try viewContext.save()
-         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-         }
-      }
-   }
-
    private func deleteBooks(offsets: IndexSet) {
       withAnimation {
          offsets.map { books[$0] }.forEach(viewContext.delete)
@@ -99,13 +81,6 @@ struct ContentView: View {
       }
    }
 }
-
-private let itemFormatter: DateFormatter = {
-   let formatter = DateFormatter()
-   formatter.dateStyle = .short
-   formatter.timeStyle = .medium
-   return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
    static var previews: some View {
